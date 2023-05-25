5269ec537f0ba5a0ff5c3d556e7294d1aae74540
Kyungchan IM <ikc6751@gmail.com>
2023-05-24 21:34:38 -0700

    Python Automation
    Python Automation

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 # ---------------------------------------------------------------------------------------------------------------- #
 ## Applicable packages for automation
 from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager          # Download chrome automatically
+from webdriver_manager.chrome import ChromeDriverManager            # Download chrome automatically
 
 from selenium.webdriver.common.by import By                         # By class are used to locate elements on a page
 from selenium.webdriver.common.keys import Keys
@@ -15,7 +15,7 @@ from selenium.webdriver.common.keys import Keys
 # ---------------------------------------------------------------------------------------------------------------- #
 ## Path to Chrome WebDriver executable
 # s = Service('/usr/local/bin/chromedriver')
-webdriver_path = '/usr/local/bin/chromedriver'                      ### Change the Path depends on the device
+# webdriver_path = '/usr/local/bin/chromedriver'                      ### Change the Path depends on the device
 
 # ---------------------------------------------------------------------------------------------------------------- #
 ## Options for Chrome Browser
@@ -25,21 +25,24 @@ options.add_argument("--start-maximized")                           # Maximize t
 
 # ---------------------------------------------------------------------------------------------------------------- #
 # Create a new instance of Chrome WebDriver
-# driver = webdriver.Chrome(ChromeDriverManager().install())        # Download automatically Chromedriver.
-driver = webdriver.Chrome(webdriver_path, options=options)
+driver = webdriver.Chrome(service=ChromeDriverManager().install(), options=options)        # Download automatically Chromedriver.
+# driver = webdriver.Chrome(webdriver_path, options=options)
 
 driver.get('https://www.linkedin.com/checkpoint/lg/sign-in-another-account')      # Go to LinkedIn website
 driver.find_element(By.ID, 'username').send_keys('Noah.wolfe3@gcu.edu')           # Type ID & Password
 driver.find_element(By.ID, 'password').send_keys('Canyon1949!')
 
 driver.find_element(By.XPATH, "//button[normalize-space()='Sign in']").click()    # Button Click
+driver.implicitly_wait(4)
+driver.find_element(By.LINK_TEXT, 'Sales Nav').send_keys(Keys.RETURN)
+driver.implicitly_wait(4)
 
 # Close the browser window
 # driver.quit()
 
 
 # ADD MORE (Look at prevoius CSV to find)
-title_list = ['CEO','Co-Founder','Business Owner','Director Contract Manufacturing','Owner','COO','Principal Engineer','Founder','Sales Supervisor','Sales Closer','Sales Representative','Director of Operations','Principal','President of Operations','Chief Executive Officer','CTO','Business Development Lead','Marketing Executive']
+# title_list = ['CEO','Co-Founder','Business Owner','Director Contract Manufacturing','Owner','COO','Principal Engineer','Founder','Sales Supervisor','Sales Closer','Sales Representative','Director of Operations','Principal','President of Operations','Chief Executive Officer','CTO','Business Development Lead','Marketing Executive']
 
 
 
