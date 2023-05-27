c42f27fe7014edcfbc861dbbd72157e9f8d4b92f
Kyungchan IM <ikc6751@gmail.com>
2023-05-27 00:27:09 -0700

    main.py update
    Exception Case Editted

--- test.py
+++ test.py
@@ -11,9 +11,6 @@ import os
 import time
 import pandas as pd
 import numpy as np
-
-# Package for scraping
-from bs4 import BeautifulSoup
 # ---------------------------------------------------------------------------------------------------------------- #
 ## Applicable packages for automation
 # Selenium module imports: pip install selenium
@@ -23,15 +20,18 @@ from webdriver_manager.firefox import GeckoDriverManager                        
 
 # Packages for web control
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait as WDW
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 # ---------------------------------------------------------------------------------------------------------------- #
 ## Options for Firefox Browser
 options = webdriver.FirefoxOptions()
 options.accept_insecure_certs = True
-# options.add_argument('--started-maximized')
-# options.add_argument('--ignore-certificate-errors')
+
 
 # ---------------------------------------------------------------------------------------------------------------- #
 ## Create a new instance of Firefox WebDriver
@@ -45,32 +45,18 @@ driver.find_element(By.ID, 'password').send_keys('Canyon1949!')
 # ---------------------------------------------------------------------------------------------------------------- #
 ## Sign in button click
 driver.find_element(By.XPATH, "//button[normalize-space()='Sign in']").click()      # Button Click
-time.sleep(10)
+time.sleep(15)
 driver.get('https://www.linkedin.com/sales/search/people')
 wait.until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search keywords']"))) # Wait until page load
 
 # ---------------------------------------------------------------------------------------------------------------- #
 ## Type the company name
 # Read CSV file (Make sure it is right csv)
-# fulldf = pd.read_csv('/home/lettuce/WorkCode/SalesNavigator/linkedin_from_excel/company_result/company_100_500.csv')
 current_dir = os.getcwd()                                                           # Get the current directory
 file_path = current_dir + "/company_result/company_100_500.csv"                     # File name
 # file_path = "/home/lettuce/WorkCode/SalesNavigator/linkedin_from_excel/company_result/company_100_500.csv"
 fulldf = pd.read_csv(file_path)
 dfcompany = fulldf["Company"]
-
-# ---------------------------------------------------------------------------------------------------------------- #
-# First Itteration on Homepage
-# temp_compamny = dfcompany[0]
-# print(temp_compamny)
-# # temp_compamny = "Liquid Foundation"
-# driver.find_element(By.XPATH, "//input[@placeholder='Search keywords']").send_keys(temp_compamny)   # Search bar click and type 
-# time.sleep(4)
-# driver.find_element(By.XPATH, "//input[@placeholder='Search keywords']").send_keys(Keys.ENTER)
-# time.sleep(4)
-# text = driver.find_element(By.XPATH, "//body/main[@role='main']/div/div/div/div/ol/li[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]").text
-# time.sleep(4)
-# print(text)
 
 
 # ---------------------------------------------------------------------------------------------------------------- #
@@ -82,46 +68,55 @@ dfcompany = fulldf["Company"]
 for x in range(len(dfcompany)):
     print(dfcompany[x])
     temp_compamny = dfcompany[x]
+
+    try:
+        # Wait until the presence of Search Bar element. 
+        wait.until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search keywords']"))).send_keys(temp_compamny, Keys.ENTER)      # Search bar click and type  
+
+        # Wait until the profile element is presence
+        text_obj = wait.until(EC.presence_of_element_located((By.XPATH, "//body/main[@role='main']/div/div/div/div/ol/li[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]")))
     
-    # Wait until the presence of Search Bar element. 
-    wait.until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search keywords']"))).send_keys(temp_compamny, Keys.ENTER)      # Search bar click and type  
+        # Scroll
+            #Click Page
+        driver.find_element(By.XPATH, "//div[@id='search-results-container']").click()
+            #Get Driver
+        actions = ActionChains(driver)
+            #Scroll Through Page
+        for _ in range(10):  # scroll ammount
+            actions.send_keys(Keys.PAGE_DOWN).perform()
+            time.sleep(.3) # scroll time
+        
+        # Loop Through People (24 per page: Maximum element in the page)
+        # for x in range(1, 24):
+        # Find the count of <li> elements
+        li_count = len(driver.find_elements(By.XPATH, "//ol/li"))
+        print(li_count)
+        # Define the maximum number of iterations
+        max_iterations = min(li_count, 24)
 
-    # Wait until the profile element is presence
-    text_obj = wait.until(EC.presence_of_element_located((By.XPATH, "//body/main[@role='main']/div/div/div/div/ol/li[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]")))
-    
-    # Finds the text object from the profile and save it as string obj
-    # position = text_obj.find_element(By.XPATH, "//body/main[@role='main']/div/div/div/div/ol/li[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]").text
-    # position = text_obj.find_element(By.XPATH, "//body/main[@role='main']/div/div/div/div/ol/li[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/span[1]").text
-    # company = text_obj.find_element(By.XPATH, "//body/main[@role='main']/div/div/div/div/ol/li[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/span[2]").text
-    # print("Position: " + position, "|", "Company: " + company)                                 # Print out the text in terminal 
+        for x in range(1, max_iterations + 1):
+            try:
+                # Define XPATH for company element and position element
+                company_obj_xpath = f"//body/main[@role='main']/div/div/div/div/ol/li[{x}]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]"
+                position_xpath = f"//body/main[@role='main']/div/div/div/div/ol/li[{x}]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/span[1]"
 
-    # Loop Through People (24 per page ish)
-    company_obj = text_obj.find_element(By.XPATH, "//body/main[@role='main']/div/div/div/div/ol/li[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]").text
-    position = text_obj.find_element(By.XPATH, "//body/main[@role='main']/div/div/div/div/ol/li[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/span[1]").text
-    company = company_obj.replace(position+" ", "")
-    print("Position: " + position, "|", "Company: " + company)                                 # Print out the text in terminal     
-    # print(position)
+                # Grab a text file from company object
+                company_obj = wait.until(EC.presence_of_element_located((By.XPATH, company_obj_xpath))).text
+                position = wait.until(EC.presence_of_element_located((By.XPATH, position_xpath))).text
+                company = company_obj.replace(position+" ", "")
+            # ------ After  : Do comparison action here ------ #
+            
+            
+                print("Position: " + position, "|", "Company: " + company)                                 # Print out the text in terminal     
+            # ------ Before : Do comparison action here ------ #
+            except NoSuchElementException:
+                break
 
-    driver.back()                               # Go to the back of the page
-    
-
-    # driver.find_element(By.XPATH, "//input[@placeholder='Search keywords']").send_keys(temp_compamny)     
-    # driver.find_element(By.XPATH, "//input[@placeholder='Search keywords']").send_keys(Keys.ENTER)        # Search bar click and type
-
-
-    #COMPARE THE COMPANY NAME WITH THE ELEMENT
-    #IF TRUE
-        #ADD TO ARRAY
-        #Move on to next person
-    #IF FALSE
-        #Move on to next person
-
-    #IF NO PERSON FOUND
-        #WRITE TO CSV FILE
-    #IF PERSON FOUND BUT NOT FIT TITLE ADD IF LESS THAN 3
-        #ADD TO LIST
-    #IF PERSON FOUND GRATOR THEN 3 FILTER TITLE
-        #ADD TO LIST
+        driver.back()                               # Go to the back of the page
+        
+    except TimeoutException:
+        driver.back()                               # Go to the back of the page
+        
 
 # ADD MORE (Look at prevoius CSV to find)
 # title_list = ['CEO', 'Co-Founder', 'Business Owner', 'Director Contract Manufacturing', 'Owner', 'COO', 'Principal Engineer', 'Founder', 'Sales Supervisor', 'Sales Closer', 'Sales Representative', 'Director of Operations', 'Principal', 'President of Operations', 'Chief Executive Officer', 'CTO', 'Business Development Lead', 'Marketing Executive', 'CEO', 'Chief Executive Officer', 'C.E.O.', 'Founder', 'Co-founder', 'Cofounder', 'Director of Development', 'Business Development', 'Retail', 'ecommerce', 'Digital Marketing', 'CMO', 'Chief Marketing Officer', 'CTO', 'Chief Technology Officer', 'Director of Marketing', 'Software', 'Fundraising', 'Partner', 'Donor', 'President', 'Owner', 'Partnership', 'Marketing manager']
